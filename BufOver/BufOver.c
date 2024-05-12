3e7455cbd70e6653716ff8aac3105b63fdebed36
Repy <gamecube02+git@gmail.com>
2024-05-12 19:56:19 +0900

    バッファオーバーフロー演習

---
+++ BufOver/BufOver.c
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char str1[8];
+	char str2[8];
+	printf("address str1:%p\n", &str1);
+	printf("address str2:%p\n", &str2);
+
+	printf("入力してください str1:");
+	scanf("%s", str1);
+	getchar();
+	printf("入力してください str2:");
+	scanf("%s", str2);
+	getchar();
+	printf("出力 str1:%s\n出力 str2:%s\n", str1, str2);
+	getchar();
+}
